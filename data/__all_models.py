750a9e15bd3f7fd19b40d66aeb82b8fac357a0ff
Denittka <dan.belokon@gmail.com>
2021-09-04 15:31:22 +0300

    Update models

--- data/__all_models.py
+++ data/__all_models.py
@@ -1,1 +1,37 @@
-from sqlalchemy import Columnm Integer, String
+from sqlalchemy import Column, Integer, Text, DateTime, String
+from sqlalchemy_serializer import SerializerMixin
+from .db_session import SqlAlchemyBase
+from flask_login import UserMixin
+
+
+class User(UserMixin, SqlAlchemyBase, SerializerMixin):
+	__tablename__ = "users"
+	id = Column(Integer, primary_key=True, autoincrement=True)
+	name = Column(String)
+	email = Column(String)
+	descritpion = Column(Text)
+	posts = Column(String)
+	likes = Column(String)
+	follows = Column(String)
+
+
+class Post(UserMixin, SqlAlchemyBase, SerializerMixin):
+	__tablename__ = "posts"
+	id = Column(Integer, primary_key=True, autoincrement=True)
+	author = Column(Integer)
+	text = Column(Text)
+	datetime = Column(DateTime)
+	comments = Column(String)
+	refers = Column(Integer)
+	likes = Column(String)
+	dislikes = Column(String)
+	ideas = Column(String)
+
+
+class Idea(UserMixin, SqlAlchemyBase, SerializerMixin):
+	__tablename__ = "ideas"
+	id = Column(Integer, primary_key=True, autoincrement=True)
+	posts = Column(String)
+	likes = Column(String)
+	dislikes = Column(String)
+	authors = Column(String)
