95206353918c9955251df85e69f87ae72eff0cc8
AzyaFLEX <AzyaDr@yandex.ru>
2021-09-24 15:25:43 +0300

    design 0.2.3.2 version: continue to write validation

--- pages/system.py
+++ pages/system.py
@@ -11,11 +11,17 @@ blueprint = flask.Blueprint(
 )
 
 
-def valid(email):
+def valid_email_(email):
     regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
     if re.fullmatch(regex, email):
         return True
     return False
+
+
+def valid_login_(login):
+    if any([elm in login for elm in ' !@"#№$;%^:&?*()-+={}[]./\'<>~']) and len(login) < 3:
+        return False
+    return True
 
 
 @blueprint.route('/system/email/<string:user_email>', methods=['GET', 'POST'])
@@ -28,6 +34,21 @@ def is_email_used(user_email):
 
 @blueprint.route('/system/valid_email/<string:user_email>', methods=['GET', 'POST'])
 def valid_email(user_email):
-    if valid(user_email):
+    if valid_email_(user_email):
+        return 'true'
+    return 'false'
+
+
+@blueprint.route('/system/login/<string:user_login>', methods=['GET', 'POST'])
+def is_login_used(user_login):
+    session = db_session.create_session()
+    if session.query(User).filter(User.name == user_login).first():
+        return 'used'
+    return 'unused'
+
+
+@blueprint.route('/system/valid_login/<string:user_login>', methods=['GET', 'POST'])
+def valid_login(user_login):
+    if valid_login_(user_login):
         return 'true'
     return 'false'
--- static/script.js
+++ static/script.js
@@ -4,16 +4,35 @@ function check(id) {
     var br_ = document.createElement('br');
     var input_ = document.getElementById(id);
 
-    if ((input_.value != '') && (request('http://127.0.0.1:5000/system/valid_email/' + input_.value) == 'true')) {
-        if (request('http://127.0.0.1:5000/system/email/' + input_.value) == 'unused') {
-            tab_error('1')
+    if (id == 'email') {
+        if ((input_.value != '') && (request('http://127.0.0.1:5000/system/valid_email/' + input_.value) == 'true')) {
+            if (request('http://127.0.0.1:5000/system/email/' + input_.value) == 'unused') {
+                tab_success(id);
+
+            }
+            else {
+                tab_error(id, 'is already in use')
+            }
         }
         else {
-            tab_error(id, 'is already in use')
+            tab_error(id, 'incorrect data for ' + id)
+        }
+    }
+    else if (id == 'login') {
+        if ((input_.value != '') && (request('http://127.0.0.1:5000/system/valid_login/' + input_.value) == 'true')) {
+            if (request('http://127.0.0.1:5000/system/login/' + input_.value) == 'unused') {
+                tab_success(id);
+            }
+            else {
+                tab_error(id, 'is already in use')
+            }
+        }
+        else {
+            tab_error(id, 'incorrect data for ' + id)
         }
     }
     else {
-        tab_error(id, 'incorrect data for ' + id)
+        // #TODO password
     }
 }
 
@@ -75,17 +94,17 @@ function tab_error(id, message) {
     console_.appendChild(script);
 }
 
-function tab_success(next_id) {
+function tab_success(id) {
     var console_ = document.getElementById('console');
     var txt = document.createElement('span');
     var br_ = document.createElement('br');
-    var input_ = document.getElementById('email');
+    var input_ = document.getElementById(id);
 
-    txt.innerHTML = input_.value+' -> ' + message;
-    txt.classList.add("main-warning");
+    txt.innerHTML = input_.value+' -> correct ' + id + '!';
+    txt.classList.add("main-success");
     console_.appendChild(txt);
 
-    input_.outerHTML = "<span>"+input_.value+"</span><br>";
+    input_.readOnly = true;;
     console_.appendChild(br_);
 
     var script = document.getElementById('script');
@@ -94,19 +113,35 @@ function tab_success(next_id) {
 
     var input_ = document.createElement('input');
     var doll__ = document.createElement('span');
+    var text__ = document.createElement('span');
+    var new_id;
+
     doll__.classList.add("mr-1");
     input_.classList.add("main-input-auth");
     input_.classList.add("pl-0");
-    input_.id = 'email';
-    doll__.innerHTML = '$';
+    if (id == 'email') {
+         new_id = 'login';
+    }
+    else if (id == 'login') {
+         new_id = 'password';
+    }
+    else {
 
-    console_.appendChild(doll__);
-    console_.appendChild(input_);
+    }
+    if (id != 'password') {
+        input_.id = new_id;
+        text__.innerHTML = document.getElementById('email').value + ' ' + input_.id.toUpperCase() + ' ~<br>';
+        doll__.innerHTML = '$';
 
-    document.getElementById("email").focus();
+        console_.appendChild(text__);
+        console_.appendChild(doll__);
+        console_.appendChild(input_);
 
-    var script = document.createElement('script');
-    script.innerHTML = 'tab("'+ id + '")';
-    script.id = 'script';
-    console_.appendChild(script);
+        document.getElementById(new_id).focus();
+
+        var script = document.createElement('script');
+        script.innerHTML = 'tab("'+ new_id + '")';
+        script.id = 'script';
+        console_.appendChild(script);
+    }
 }
